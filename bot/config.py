f29f41504db4132b6305a7c113744e7bca3445f4
subhranta5 <81546243+subhranta5@users.noreply.github.com>
2021-09-22 16:47:27 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,26 +6,26 @@ class Config(object):
     # You can keep this default
     SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
     # Put MongoDB URL
-    DATABASE_URL = get_config("DATABASE_URL", "")
+    DATABASE_URL = get_config("DATABASE_URL", "mongodb+srv://snpjames:NmKXHRS3kp*ggSd@cluster0.1igut.mongodb.net/cluster0?retryWrites=true&w=majority")
     # get a token from @BotFather
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "1813311241:AAH8I-1aRdP3hVSv7Uj03dT8r_a2fDkIxrI")
     # The Telegram API things
-    APP_ID = int(get_config("APP_ID", 12345))
-    API_HASH = get_config("API_HASH", "")
-    LOG_CHANNEL = get_config("LOG_CHANNEL")
-    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+    APP_ID = int(get_config("APP_ID", 3566549))
+    API_HASH = get_config("API_HASH", "829977f2da962ae570aa0b91572aa8e4")
+    LOG_CHANNEL = get_config("LOG_CHANNEL", "-1001333210651")
+    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", "vidcompro") # Without `@` LOL
      # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
     AUTH_USERS = set(
         int(x) for x in get_config(
-            "AUTH_USERS",
+            "AUTH_USERS", "461650361 -1001333210651"
             should_prompt=True
         ).split()
     )
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
-    BOT_USERNAME = get_config("BOT_USERNAME", "")
+    BOT_USERNAME = get_config("BOT_USERNAME", "@Vidcompros_bot")
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
